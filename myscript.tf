9543228067482762cd68f12fcf60545babaa95ac
NanduriHyndavi <122361579+NanduriHyndavi@users.noreply.github.com>
2023-01-26 19:34:19 +0530

    Add files via upload

---
+++ myscript.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+access_key = "AKIA2E76B54NCHSZFZSI"
+secret_key = "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"
+region = "us-east-1"
+}
+
+resource "aws_instance" "terraform_demo" {
+ami = "ami-00874d747dde814fa"
+instance_type = "t2.micro"
+subnet_id= "subnet-0fe125630b1249d3b"
+security_groups= "[sg-0e497789aa437952f]"
+
+tags= {
+    Name= "Myinstance"
+}
+}
