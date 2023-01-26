364c6545a66a6c09212916ccb99b8e300bcba684
NanduriHyndavi <122361579+NanduriHyndavi@users.noreply.github.com>
2023-01-26 21:21:58 +0530

    Update myscript.tf

--- myscript.tf
+++ myscript.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "terraform_demo" {
 ami = "ami-00874d747dde814fa"
 instance_type = "t2.micro"
 subnet_id= "subnet-0fe125630b1249d3b"
-security_groups= "[sg-0e497789aa437952f]"
+security_groups= ["sg-0e497789aa437952f"]
 
 tags= {
     Name= "Myinstance"
